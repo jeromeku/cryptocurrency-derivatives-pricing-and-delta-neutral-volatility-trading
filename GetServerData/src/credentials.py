71abc7fc81e1e3a16ae22a87670c7236c4194004
Matteo Bottacini <matteo.bottacini@usi.ch>
2021-09-17 17:07:01 +0200

    Update credentials.py

--- GetServerData/src/credentials.py
+++ GetServerData/src/credentials.py
@@ -1,7 +1,7 @@
 """ User credentials to SSH into the remote server """
 
 # complete with your credentials
-host = "dev02.petergruber.com"
+host = "*******.com"
 port = 22
 username = "bottama"
-password = "1234"
+password = "****"
